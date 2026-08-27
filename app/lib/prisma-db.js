951a087cddc03b0b67ca8a686b83acf131f9ff74
Ambre <olambre7@gmail.com>
2026-08-27 16:10:00 +0200

    Fin d'installation pour utiliser la BDD prisma.

---
+++ app/lib/prisma-db.js
@@ -0,0 +1,21 @@
+import { PrismaClient } from '../generated/prisma/client';
+
+const prisma = new PrismaClient();
+
+export const getAllPhotographers = () => prisma.photographer.findMany();
+
+export const getPhotographer = (id) =>
+  prisma.photographer.findUnique({
+    where: { id },
+  });
+
+export const getAllMediasForPhotographer = (photographerId) =>
+  prisma.media.findMany({
+    where: { photographerId },
+  });
+
+export const updateNumberOfLikes = (mediaId, newNumberOfLikes) =>
+  prisma.media.update({
+    where: { id: mediaId },
+    data: { likes: newNumberOfLikes },
+  });
