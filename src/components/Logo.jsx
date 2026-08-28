73a83d46172a74bd5c81b5db1475afa685ca8e08
Ambre <olambre7@gmail.com>
2026-08-28 15:16:14 +0200

    Logo done, passont à la photo de profil

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,26 @@
+import styled from "styled-components";
+import Image from "next/image";
+import Link from "next/link";
+
 const Logo = () => {
     return (
-        <div>
-            <h2>Logo</h2>
-            <p>This is a simple logo component.</p>
-        </div>
+        <HomeLink>
+            <Link href="/">
+                <Image 
+                src="/logo.png" 
+                    alt="Logo" 
+                    width={100} 
+                    height={100} 
+                />
+            </Link>
+        </HomeLink>
     );
 };
 
 export default Logo;
+
+const HomeLink = styled.div`
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+`
